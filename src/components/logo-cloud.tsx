af59f54c03c700ed5e0c4e396f176f17f4a5c1f7
RafifMMR <rafifmuhammad2003@gmail.com>
2025-07-24 14:14:21 +0700

    add section logo stack and tools

--- src/components/logo-cloud.tsx
+++ src/components/logo-cloud.tsx
@@ -1,40 +1,107 @@
-import { clsx } from 'clsx'
+'use client'
 
-export function LogoCloud({
-  className,
-}: React.ComponentPropsWithoutRef<'div'>) {
+import { InfiniteSlider } from '@/components/ui/infinite-slider'
+import { ProgressiveBlur } from '@/components/ui/progressive-blur'
+
+const logos = [
+  {
+    src: 'https://res.cloudinary.com/dnuusu1kb/image/upload/v1751958651/react-original.svg',
+    alt: 'GitHub',
+    height: 50,
+  },
+  {
+    src: 'https://res.cloudinary.com/dnuusu1kb/image/upload/v1751958251/typescript-original.svg',
+    alt: 'Nvidia',
+    height: 50,
+  },
+  {
+    src: 'https://res.cloudinary.com/dnuusu1kb/image/upload/v1751958648/tailwindcss-original.svg',
+    alt: 'Column',
+    height: 50,
+  },
+  {
+    src: 'https://res.cloudinary.com/dnuusu1kb/image/upload/v1751960617/nextjs-dark-original.svg',
+    alt: 'Nike',
+    height: 50,
+  },
+  {
+    src: 'https://res.cloudinary.com/dnuusu1kb/image/upload/v1752025357/shadcn-ui-light-original.svg',
+    alt: 'GitHub',
+    height: 50,
+  },
+  {
+    src: 'https://res.cloudinary.com/dnuusu1kb/image/upload/v1752029833/chatgpt-light-original.svg',
+    alt: 'Lemon Squeezy',
+    height: 50,
+  },
+  {
+    src: 'https://res.cloudinary.com/dnuusu1kb/image/upload/v1752029960/figma-original.svg',
+    alt: 'Laravel',
+    height: 50,
+  },
+  {
+    src: 'https://res.cloudinary.com/dnuusu1kb/image/upload/v1752029479/vercel-light-original.svg',
+    alt: 'Lilly',
+    height: 50,
+  },
+  {
+    src: 'https://res.cloudinary.com/dnuusu1kb/image/upload/v1752027524/solidity-light-original.svg',
+    alt: 'OpenAI',
+    height: 50,
+  },
+]
+
+export function LogoCloud() {
   return (
-    <div
-      className={clsx(
-        className,
-        'flex justify-between max-sm:mx-auto max-sm:max-w-md max-sm:flex-wrap max-sm:justify-evenly max-sm:gap-x-4 max-sm:gap-y-4',
-      )}
-    >
-      <img
-        alt="SavvyCal"
-        src="/logo-cloud/savvycal.svg"
-        className="h-9 max-sm:mx-auto sm:h-8 lg:h-12"
-      />
-      <img
-        alt="Laravel"
-        src="/logo-cloud/laravel.svg"
-        className="h-9 max-sm:mx-auto sm:h-8 lg:h-12"
-      />
-      <img
-        alt="Tuple"
-        src="/logo-cloud/tuple.svg"
-        className="h-9 max-sm:mx-auto sm:h-8 lg:h-12"
-      />
-      <img
-        alt="Transistor"
-        src="/logo-cloud/transistor.svg"
-        className="h-9 max-sm:mx-auto sm:h-8 lg:h-12"
-      />
-      <img
-        alt="Statamic"
-        src="/logo-cloud/statamic.svg"
-        className="h-9 max-sm:mx-auto sm:h-8 lg:h-12"
-      />
-    </div>
+    <section className="overflow-hidden py-16">
+      <div className="relative m-auto flex max-w-7xl flex-col items-center gap-6 px-6 md:flex-row md:items-start">
+        {/* Caption */}
+        <div className="mb-4 w-full text-center md:mb-0 md:w-48 md:shrink-0 md:pr-6 md:text-end">
+          <div className="mx-auto border-b pb-6 md:mx-0 md:max-w-44 md:border-r md:pr-6">
+            <p className="text-sm">
+              We use these tools and stacks to build our products.
+            </p>
+          </div>
+        </div>
+
+        {/* Logos */}
+        <div className="relative w-full">
+          <InfiniteSlider speedOnHover={20} speed={40} gap={112}>
+            {logos.map((logo, i) => (
+              <div
+                key={i}
+                className="group relative flex items-center justify-center"
+              >
+                <img
+                  className="h-auto dark:invert"
+                  src={logo.src}
+                  alt={`${logo.alt} Logo`}
+                  style={{ height: `${logo.height}px` }}
+                />
+                <span className="absolute bottom-full mb-2 hidden rounded-md bg-black px-2 py-1 text-xs text-white opacity-0 transition-opacity group-hover:opacity-100">
+                  {logo.alt}
+                </span>
+              </div>
+            ))}
+          </InfiniteSlider>
+
+          {/* Gradient edges */}
+          <div className="absolute inset-y-0 left-0 w-20 bg-gradient-to-r from-background"></div>
+          <div className="absolute inset-y-0 right-0 w-20 bg-gradient-to-l from-background"></div>
+
+          {/* Blur effect */}
+          <ProgressiveBlur
+            className="pointer-events-none absolute left-0 top-0 h-full w-20"
+            direction="left"
+            blurIntensity={1}
+          />
+          <ProgressiveBlur
+            className="pointer-events-none absolute right-0 top-0 h-full w-20"
+            direction="right"
+            blurIntensity={1}
+          />
+        </div>
+      </div>
+    </section>
   )
 }
